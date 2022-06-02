60ea5cfdcb6a23f056b380e840c6e322a058ca9d
philipn778 <philipncontact@gmail.com>
2022-06-02 21:13:44 +0000

    Changes to be committed: 	modified:   script.js

--- script.js
+++ script.js
@@ -23,9 +23,19 @@ function setNextQuestion () {
 
 function showQuestion(question) {
     questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    }}
 }
 
-function selectAnswer () {
+function selectAnswer (e) {
 
 }
 
