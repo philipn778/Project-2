b3e3e500b33044594947338c06b9e3fce6b60971
philipn778 <philipncontact@gmail.com>
2022-06-02 06:28:43 +0000

    Changes to be committed: 	modified:   assets/css/style.css 	modified:   index.html 	modified:   script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex 
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+console.log('Started')
+startButton.classList.add('hide')
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
+questionContainerElement.classList.remove('hide')
+setNextQuestion()
+}
+
+function setNextQuestion () {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+}
+
+function selectAnswer () {
+
+}
+
+const questions = [
+    {
+        question: 'Which is europes highest building?',
+        answers: [
+            {text: 'Lakhta Center', correct: true},
+            {text: 'Big Ben', correct: false},
+            {text: 'Eiffel Tower', correct: false},
+            {text: 'Turning Torso', correct: false}
+        ]
+    }
+]
